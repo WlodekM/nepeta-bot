01f3cba0fb6641a1e92cea3d2edf130a9daefdbc
WlodekM <wlodzio.maly@gmail.com>
2025-10-26 15:09:42 +0200

    timestamps

--- commands/work.ts
+++ commands/work.ts
@@ -13,11 +13,11 @@ export default {
 		if (user.data.storage.work_cooldown &&
 			user.data.storage.work_cooldown > Date.now()
 		)
-			return message.reply(`woah, chill out, no one wants to work as a minimum-wage slave *that* much. come back at ${user.data.storage.work_cooldown} millisecond unix`);
+			return message.reply(`woah, chill out, no one wants to work as a minimum-wage slave *that* much. come back at ${user.data.storage.work_cooldown} millisecond unix (that's <t:${Math.floor(user.data.storage.work_cooldown/1000)}> for you non-nerds)`);
 		user.data.money += sallarries[user.data.storage.job!] ?? 20;
 		user.data.storage.work_cooldown = Date.now() + (1000*60*30);
 		await user.update();
 		return message.reply(`you worked for half an hour, you got ${sallarries[user.data.storage.job!] ?? 20} money, you now have ${user.data.money} money.\
- you can work again at ${user.data.storage.work_cooldown} millisecond unix`);
+ you can work again at ${user.data.storage.work_cooldown} millisecond unix (that's <t:${Math.floor(user.data.storage.work_cooldown/1000)}> for you non-nerds)`);
 	}
 } as Command;
